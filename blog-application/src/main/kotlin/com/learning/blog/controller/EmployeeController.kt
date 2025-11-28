1dbf5ff5436b8852071118dd2ceb908e320cd49d
Muhammad Azharul Islam <azadbasis@gmail.com>
2025-11-28 07:27:28 +0600

    Employee controller GetAllUsers and GetUserById implemented.

--- blog-application/src/main/kotlin/com/learning/blog/controller/EmployeeController.kt
+++ blog-application/src/main/kotlin/com/learning/blog/controller/EmployeeController.kt
@@ -6,38 +6,34 @@ import com.learning.blog.model.EmployeeResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/employee")
+@RequestMapping("/api/employees")
 class EmployeeController {
-    val employees= mutableListOf<Employee>()
-    val currentId=1L
+
+    private val employees=mutableListOf<Employee>()
+    private var currentId=1L
 
     @GetMapping()
-    fun getAllEmployee(): ResponseEntity<APIResponse<List<EmployeeResponse>>>{
-        val employeeResponses=employees.map { employee ->
-            EmployeeResponse.fromEmployee(employee)
+    fun getAllEmployees(): ResponseEntity<APIResponse<List<EmployeeResponse>>>{
+        val employeeResponse=employees.map{ EmployeeResponse.fromEmployee(it) }
+        return if(employeeResponse.isNotEmpty()){
+            ResponseEntity.ok(APIResponse(success = true, message = "Fetch employee data",data=employeeResponse))
+        }else{
+             ResponseEntity.status(HttpStatus.NOT_FOUND).body(APIResponse(success = false, message = "No data found",data = null))
         }
 
-        return if (employeeResponses.isNotEmpty()){
-            ResponseEntity.ok(
-                APIResponse(
-                    success = true,
-                    message="Employee found",
-                    data= employeeResponses
-                )
-            )
+    }
+    @GetMapping("/{id}")
+    fun getUserById(@PathVariable id:Long):ResponseEntity<APIResponse<EmployeeResponse>>{
+        val employee=employees.find{it.id==id}
+        return if(employee!=null){
+            ResponseEntity.ok(APIResponse(success = true, message = "Employee found successfully",data= EmployeeResponse.fromEmployee(employee)))
         }else{
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                APIResponse(
-                    success = false,
-                    message="No employee found",
-                    data= null
-                )
-
-            )
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(APIResponse(success = false, message = "Employee with id $id not found", data = null))
         }
     }
 
--- blog-application/src/main/kotlin/com/learning/blog/model/EmployeeModel.kt
+++ blog-application/src/main/kotlin/com/learning/blog/model/EmployeeModel.kt
@@ -2,13 +2,33 @@ package com.learning.blog.model
 
 import java.time.LocalDateTime
 
-data class Employee(val id: Long,val name:String,val email:String,val password:String,val department: String,val createdAt: LocalDateTime= LocalDateTime.now())
-data class APIResponse<T>(val success: Boolean,val message:String,val data:T?=null,val timestamp:LocalDateTime= LocalDateTime.now())
-data class CreateEmployeeRequest(val name: String,val email: String,val password: String,val department: String)
-data class UpdateEmployeeRequest(val name: String,val email:String,val department: String)
-data class EmployeeResponse(val id: Long,val name:String,val email:String,val department:String,val createdAt:LocalDateTime){
-    companion object{
-        fun fromEmployee(employee:Employee)=
-            EmployeeResponse(employee.id,employee.name,employee.email,employee.department,employee.createdAt)
+data class Employee(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val password: String,
+    val department: String,
+    val createdAt: LocalDateTime = LocalDateTime.now()
+)
+
+data class APIResponse<T>(
+    val success: Boolean,
+    val message: String,
+    val data: T? = null,
+    val timestamp: LocalDateTime = LocalDateTime.now()
+)
+
+data class CreateEmployeeRequest(val name: String, val email: String, val password: String, val department: String)
+data class UpdateEmployeeRequest(val name: String, val email: String, val department: String)
+data class EmployeeResponse(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val department: String,
+    val createdAt: LocalDateTime
+) {
+    companion object {
+        fun fromEmployee(employee: Employee) =
+            EmployeeResponse(employee.id, employee.name, employee.email, employee.department, employee.createdAt)
     }
 }
